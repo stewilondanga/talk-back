c8d4df060410bb3990d20eb66b6328199598707a
Stewart ilondanga <stilondanga@gmail.com>
2018-01-02 12:01:20 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+// Handle browser prefixes
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+
+// Setup test data
+var fills = [
+    'rgba(255, 255, 255, 1)',
+    'rgba(254, 119, 132, 1)',
+    'rgba(60, 128, 219, 1)',
+    'rgba(127, 254, 146, 1)'
+];
+
 
 
 var navigate = (function() {
